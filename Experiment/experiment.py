1633fa2d6601466ca5a49708010b44f03c264061
Mohamad Alikhani <87755081+BahMoh@users.noreply.github.com>
2026-02-08 16:35:16 +0330

    Update experiment.py

--- Experiment/experiment.py
+++ Experiment/experiment.py
@@ -6,7 +6,7 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 memory_limit=4 * 1024
 tf.config.experimental.set_virtual_device_configuration(gpus[0], 
     [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
-tf.config.experimental.set_virtual_device_configuration(gpus[0], 
+tf.config.experimental.set_virtual_device_configuration(gpus[1], 
     [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
 
 from Experiment import run_maddpg

2aca36dfc324a8473e2ac84f0ebfa43ea64d14b6
Mohamad Alikhani <87755081+BahMoh@users.noreply.github.com>
2026-02-08 18:27:02 +0330

    Disable GPU memory limit configuration
    Comment out GPU memory configuration for TensorFlow.

--- Experiment/experiment.py
+++ Experiment/experiment.py
@@ -4,8 +4,8 @@ from absl import app
 import tensorflow as tf
 gpus = tf.config.experimental.list_physical_devices('GPU')
 memory_limit=4 * 1024
-tf.config.experimental.set_virtual_device_configuration(gpus[0], 
-    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
+# tf.config.experimental.set_virtual_device_configuration(gpus[0], 
+#     [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
 # tf.config.experimental.set_virtual_device_configuration(gpus[1], 
 #     [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
 
